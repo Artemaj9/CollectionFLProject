141dfaa4ff51935ed4d2ac30b64c9a32fd4c2d15
Artem <moiseev@physics.msu.ru>
2023-08-20 12:18:44 +0300

    Simple collection view

---
+++ CollectionFLProject/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionFLProject
+//
+//  Created by Artem on 20.08.2023.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var myLabel: UILabel!
+}
